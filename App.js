2cd65b7db99fa2e87c771de59a1f5812f28792a1
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2021-08-01 05:34:33 +1000

    Added reducers for global nav

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import { store } from "./store";
+import GlobalStyles from "./GlobalStyles";
 
 export default function App() {
 	return (
-		<Provider store={}>
-			<View style={styles.container}>
-				<Text>Lets build Uber!</Text>
-			</View>
+		<Provider store={store}>
+			<SafeAreaView style={GlobalStyles.droidSafeArea}>
+				<HomeScreen />
+			</SafeAreaView>
 		</Provider>
 	);
 }
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -9,4 +9,26 @@ const initialState = {
 export const navSlice = createSlice({
 	name: "nav",
 	initialState,
+	reducer: {
+		setOrigin: (state, action) => {
+			state.origin = action.payload;
+		},
+		setDestination: (state, action) => {
+			state.destination = action.payload;
+		},
+		setTravelTimeInformation: (state, action) => {
+			state.travelTimeInformation = action.payload;
+		},
+	},
 });
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+	navSlice.actions;
+
+//selectors
+export const selectOrigin = (state) => state.navSlice.origin;
+export const selectDestination = (state) => state.navSlice.destination;
+export const selectTravelTimeInformation = (state) =>
+	state.navSlice.travelTimeInformation;
+
+export default navSlice.reducer;
